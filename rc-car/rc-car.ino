73233e19d27f4ccd88eb82ab8cfa50ddf9b253f7
JC5311 <jc.psp5@gmail.com>
2019-03-05 22:04:30 -0500

    Began code segmentation for feature parallelization. Added FreeRTOS for future task-switching.

--- rc-car/rc-car.ino
+++ rc-car/rc-car.ino
@@ -1,10 +1,24 @@
+/** 
+ * The purpose of this program is to implement a "RC Effects" device which allows
+ * users to easily perform lighting and audio animation on an rc car.
+ * 
+ * Author: Juan Caraballo
+ * Created: 3/4/2019
+ * Copyright 2019, Juan Caraballo, All rights reserved.
+ * 
+ * Description: The purpose of this program is to implement a "RC Effects" 
+ * device which allows users to easily perform lighting and audio animation on
+ * an rc car.
+ * 
+*/
+
 #include <FastLED.h>
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+#include <task.h>
 
 // How many leds in your strip?
-#define NUM_LEDS_LEFT 8
-#define NUM_LEDS_RIGHT 8
-#define NUM_LEDS_FRONT 8
-#define NUM_LEDS_REAR 8
+#define NUM_LED_PER_STRIP 8
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -14,59 +28,100 @@
 #define CAR_FRONT_PIN 5
 #define CAR_REAR_PIN 4
 
-// Define the array of leds
-CRGB leds_left[NUM_LEDS_LEFT];
-CRGB leds_right[NUM_LEDS_RIGHT];
-CRGB leds_front[NUM_LEDS_FRONT];
-CRGB leds_rear[NUM_LEDS_REAR];
+// Define the arrays of leds
+CRGB leds_left[NUM_LED_PER_STRIP];
+CRGB leds_right[NUM_LED_PER_STRIP];
+CRGB leds_front[NUM_LED_PER_STRIP];
+CRGB leds_rear[NUM_LED_PER_STRIP];
 
+/* RTOS Dependent Globals */
+TaskHandle_t xAnimateHandle;
+
+/* Function Prototypes */
+void AnimationTask(void *pvParameters __attribute__((unused)));
+
+/* Functional Code */
 void setup() { 
 	Serial.begin(57600);
-	LEDS.addLeds<NEOPIXEL,CAR_LEFT_PIN>(leds_left,NUM_LEDS_LEFT);
-  LEDS.addLeds<NEOPIXEL,CAR_RIGHT_PIN>(leds_right,NUM_LEDS_RIGHT);
-  LEDS.addLeds<NEOPIXEL,CAR_FRONT_PIN>(leds_front,NUM_LEDS_FRONT);
-  LEDS.addLeds<NEOPIXEL,CAR_REAR_PIN>(leds_rear,NUM_LEDS_REAR);
+	LEDS.addLeds<NEOPIXEL,CAR_LEFT_PIN>(leds_left,NUM_LED_PER_STRIP);
+  LEDS.addLeds<NEOPIXEL,CAR_RIGHT_PIN>(leds_right,NUM_LED_PER_STRIP);
+  LEDS.addLeds<NEOPIXEL,CAR_FRONT_PIN>(leds_front,NUM_LED_PER_STRIP);
+  LEDS.addLeds<NEOPIXEL,CAR_REAR_PIN>(leds_rear,NUM_LED_PER_STRIP);
 	LEDS.setBrightness(128);
+
+	//Configure RTOS Tasks
+	xTaskCreate(
+		AnimationTask,
+    (const portCHAR *)"Animation Handler",
+    128, //stack size
+    NULL,
+    1, //priority
+    &xAnimateHandle
+	);
 }
 
 void fadeall()
 { 
-  for(int i = 0; i < NUM_LEDS_LEFT; i++) 
+  for(int i = 0; i < NUM_LED_PER_STRIP; i++) 
   { 
     leds_left[i].fadeToBlackBy(50);
     leds_right[i].fadeToBlackBy(50); 
-    //leds_front[i].fadeToBlackBy(50); 
-    //leds_rear[i].fadeToBlackBy(50); 
+    leds_front[i].fadeToBlackBy(50); 
+    leds_rear[i].fadeToBlackBy(50); 
   } 
 }
 
-void loop() { 
-	// First slide the led in one direction
-  int i, j;
-	for(i = 0,j = NUM_LEDS_RIGHT-1; i < NUM_LEDS_LEFT && j >= 0; i++, j--) {
-		// Set the i'th led to red 
-		leds_left[i] = CRGB::Purple;
-    leds_right[j] = CRGB::Aqua;
-		// Show the leds
-		FastLED.show(); 
-		// now that we've shown the leds, reset the i'th led to black
-		// leds[i] = CRGB::Black;
-		fadeall();
-		// Wait a little bit before we loop around and do it again
-		delay(50);
-	}
+void loop()
+{
+	//all work is done within RTOS tasks
+}
 
-	// Now go in the other direction.
-	for(i = (NUM_LEDS_LEFT)-1, j = 0; i >= 0 && j < NUM_LEDS_RIGHT; i--, j++) {
-		// Set the i'th led to red 
-		leds_left[i] = CRGB::Aqua;
-    leds_right[j] = CRGB::Purple;
-		// Show the leds
-		FastLED.show();
-		// now that we've shown the leds, reset the i'th led to black
-		// leds[i] = CRGB::Black;
-		fadeall();
-		// Wait a little bit before we loop around and do it again
-		delay(50);
+/* RTOS Tasks */
+void AnimationTask(void *pvParameters __attribute__((unused)))
+{
+	(void) pvParameters;
+
+	while(1) //a task shall never exit or return
+	{
+		/*Beginning of single animation*/
+		//Note the whole point of setting comments for this is just to emphasize that
+		//this task will need to become more versatile in time to accept programming
+		//from externally connected devices. For now I may just have one or more
+		//animations that execute depdending on a set switch variable.
+		
+		// First slide the leds in one direction
+  	int i, j; //i and j trigger opposite flowing animation (note the counter)
+		for(i = 0,j = NUM_LED_PER_STRIP-1; i < NUM_LED_PER_STRIP && j >= 0; i++, j--)
+		{
+			//set colors
+			leds_left[i] = CRGB::Purple;
+			leds_right[j] = CRGB::Aqua;
+			leds_front[i] = CRGB::Purple;
+			leds_rear[j] = CRGB::Aqua;
+			// Show the leds
+			FastLED.show(); 
+			// start fading the leds
+			fadeall();
+			// Wait a little bit before we loop around and do it again
+			delay(50);
+		}
+
+		// Now slide them in the other direction.
+		for(i = (NUM_LED_PER_STRIP)-1, j = 0; i >= 0 && j < NUM_LED_PER_STRIP; i--, j++)
+		{
+			// Set colors
+			leds_left[i] = CRGB::Aqua;
+			leds_right[j] = CRGB::Purple;
+			leds_front[i] = CRGB::Aqua;
+			leds_rear[j] = CRGB::Purple;
+			// Show the leds
+			FastLED.show();
+			// start fading the leds
+			fadeall();
+			// Wait a little bit before we loop around and do it again
+			delay(50);
+		}
+		
+		/*End of single animation*/
 	}
-} //loop()
+}
